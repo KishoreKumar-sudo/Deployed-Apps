c1c5dc613842d2b34de3486aa28e2bf346410db5
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-02-08 20:33:34 +0530

    quotes

--- Motivation Quotes Generator/src/App.js
+++ Motivation Quotes Generator/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react'
-const App = () => {
-  return (
-    <h1>App</h1>
-  )
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css';
+
+class App extends Component {
+  state = { advice: '' }
+
+  componentDidMount(){
+    console.log('Component Did Mount');
+  }
+
+  render() {
+    return (
+      <h1>App</h1>
+    ) 
+  }
 }
+
 export default App
