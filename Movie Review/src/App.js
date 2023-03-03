0eecf804f69d7076a86d1fd7efcb5394676df570
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-03 16:33:17 +0530

    try & catch block

--- Movie Review/src/App.js
+++ Movie Review/src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
-
+import API from './API/Axiosconfig'
+import { useState, useEffect } from 'react'
 function App() {
+  //let's return a destructured array from the useState Hook
+  //for thread functionality management we're using async await
+  const [movies, setMovies] = useState();
+  
+  const getMovies = async () => {
+    try {
+      const response = await API.get("/API/v1/movies");
+      setMovies(response.data)
+    } 
+    catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div className="App">
 
