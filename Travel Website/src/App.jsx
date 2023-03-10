5ba7b64a4322052c0b6ae67d869a78eebfc65505
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-10 13:25:35 +0530

    imported

--- Travel Website/src/App.jsx
+++ Travel Website/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Cards from './Components/Cards/Cards'
+import Footer from './Components/Footer/Footer'
+import Discount from './Components/Discount/Discount'
+import Review from './Components/Review/Review'
+import Staff from './Components/Staff/Staff'
+import Tour from './Components/Tours/Tour'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
