f6229acbd33c7078ab5c9a2eab719a933a8d96dd
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-10 17:44:38 +0530

    navbar-links

--- Travel Website/src/Components/Navbar/Navbar.jsx
+++ Travel Website/src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <div className="header flex">
+        <div className="logoDiv">
+          <h3 className="logo">Wonder</h3>
+        </div>
+        <div className="menuDiv">
+          <ul className="menuLists">
+            <li className="navItem">
+              <a href="#" className="menuLinks">Home</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLinks">About Us</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLinks">Our Tours</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLinks">Gallery</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLinks">Blog</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLinks">Contact Us</a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="menuLinks">Pages</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
