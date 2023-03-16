a6d7ffa05b81df391f2e7bd4d9f7cefa77919f15
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-16 22:20:11 +0530

    alignment

--- Travel Website/src/Components/Home/Home.jsx
+++ Travel Website/src/Components/Home/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './Home.css'
+
 const Home = () => {
   return (
     <div className='home'>
+      
       <div className='homeText'>
-        <span className='spanText'>A TEAM OF PROFESSIONAL TRAVEL EXPERTS
-        </span>
+        <span className='spanText'>
+          A TEAM OF PROFESSIONAL TRAVEL EXPERTS
+          </span>
+
         <div className="homeTitle">
           Trust <strong>Our Experience</strong>
         </div>
+
         <div className="btn">Get in Touch</div>
       </div>
 
