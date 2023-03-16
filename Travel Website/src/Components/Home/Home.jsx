afb2700d609c356977f915cdd57c1756e37dfaa6
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-16 19:50:51 +0530

    home jsx

--- Travel Website/src/Components/Home/Home.jsx
+++ Travel Website/src/Components/Home/Home.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className='homeText'>
+        <span className='spanText'>
+          A TEAM OF PROFESSIONAL TRAVEL EXPERTS
+        </span>
+        <div className="homeTitle">
+          Trust <strong>Our Experience</strong>
+        </div>
+        <div className="btn">Get in Touch</div>
+      </div>
+
+    </div>
   )
 }
 
